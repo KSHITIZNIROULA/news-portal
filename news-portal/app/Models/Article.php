e2c21321a7775a51fcb741fc6e68b2d024aace64
KSHITIZNIROULA <niroulachhitiz@gmail.com>
2025-05-27 21:16:00 +0545

    feat: add models and requests for articles, categories, comments, images, and user authentication

---
+++ news-portal/app/Models/Article.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+
+    protected $fillable = ['title', 'slug', 'content', 'category_id', 'author_id', 'published_at', 'image', 'status'];
+    
+    protected $casts=[
+        'published_at' => 'datetime',
+        'status' => 'string',
+    ];
+    
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
+
+    public function comments(){
+        return $this->hasMany(Comment::class);
+    }
+    
+    public function images()
+    {
+        return $this->hasMany(Images::class);
+    }
+}
