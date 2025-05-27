d688935e906f517ee60435ec3cd32b78d0265810
KSHITIZNIROULA <niroulachhitiz@gmail.com>
2025-05-27 21:15:53 +0545

    feat: implement controllers for article, authentication, password management, and user profile

---
+++ news-portal/app/Http/Controllers/ArticleController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Article;
+use Illuminate\Http\Request;
+
+class ArticleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+{
+        $articles = Article::where('status', 'published')
+            ->with(['category', 'author'])
+            ->latest('published_at')
+            ->paginate(10);
+
+        return view('article.index',['articles' => $articles]);
+    }
+
+
+public function show(string $slug)
+    {
+        $article = Article::where('slug', $slug)
+            ->where('status', 'published')
+            ->with(['category', 'author'])
+            ->firstOrFail();
+
+        return view('articles.show',['articles' => $article]);
+    }
+
+}
