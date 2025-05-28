cb10eafe1ec1e88b19f230c1fcc3e3ffd5caef23
KSHITIZNIROULA <niroulachhitiz@gmail.com>
2025-05-28 08:18:35 +0545

    feat: add role-based middleware for user redirection in application configuration

--- news-portal/bootstrap/app.php
+++ news-portal/bootstrap/app.php
@@ -1,8 +1,10 @@
 <?php
 
+use App\Http\Middleware\RedirectBasedOnRole;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Spatie\Permission\Middleware\RoleMiddleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -10,8 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
-        //
+    ->withMiddleware(function (Middleware $middleware){
+        $middleware->alias([
+            'role'=>RoleMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
