2d415d41f5406bf7ec416c76c9deebb4a6cfb690
KSHITIZNIROULA <niroulachhitiz@gmail.com>
2025-05-28 08:18:30 +0545

    fix: correct redirection route after user registration and ensure role assignment

--- news-portal/app/Http/Controllers/Auth/RegisteredUserController.php
+++ news-portal/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -40,11 +40,13 @@ class RegisteredUserController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+        
+        $user->assignRole('user');
 
         event(new Registered($user));
 
         Auth::login($user);
 
-        return redirect(route('dashboard', absolute: false));
+        return redirect(route('home', absolute: false));
     }
 }
