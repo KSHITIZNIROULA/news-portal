703f684ca0b961a43222779330b38db54f69327d
KSHITIZNIROULA <niroulachhitiz@gmail.com>
2025-05-28 08:19:41 +0545

    Refactor code structure for improved readability and maintainability

--- news-portal/resources/views/home.blade.php
+++ news-portal/resources/views/home.blade.php
@@ -1,17 +1,15 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
+@extends('layouts.app')
+@section('content')
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
+                    <h1 class="text-2xl font-bold mb-4">HomePage</h1>
+                    <p>Home Page</p>
+                    <p>See all the articles.</p>
                 </div>
             </div>
         </div>
     </div>
-</x-app-layout>
+    
+    @endsection
