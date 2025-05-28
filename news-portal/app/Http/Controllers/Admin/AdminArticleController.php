2f3978d88fd99101773be438b41fae754315961c
KSHITIZNIROULA <niroulachhitiz@gmail.com>
2025-05-28 08:18:16 +0545

    feat: add user creation functionality with validation and role assignment

--- news-portal/app/Http/Controllers/Admin/AdminArticleController.php
+++ news-portal/app/Http/Controllers/Admin/AdminArticleController.php
@@ -5,12 +5,37 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Article;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
 
 class AdminArticleController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
+    public function createUser(){
+        return view('admin.users.create');
+    } 
+    
+    public function storeuser(Request $request){
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+            'password' => ['required', 'confirmed', Rules\Password::defaults()],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+        
+        $user->assignRole($request->role);
+
+return redirect()->route('admin.articles.index')->with('success', 'User created successfully.');
+    }
+
     public function index()
 {
         $articles = Article::with(['category', 'author'])->paginate(10);
