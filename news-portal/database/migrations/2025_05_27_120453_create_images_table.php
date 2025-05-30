e8ebb7e90fe056b5479f07370b75026d209b821a
KSHITIZNIROULA <niroulachhitiz@gmail.com>
2025-05-30 20:44:41 +0545

    fix: change 'path' column type from json to string in images table migration

--- news-portal/database/migrations/2025_05_27_120453_create_images_table.php
+++ news-portal/database/migrations/2025_05_27_120453_create_images_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
         Schema::create('images', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(Article::class)->constrained()->cascadeOnDelete();
-            $table->json('path')->nullable();
+            $table->string('path')->nullable();
             $table->timestamps();
         });
     }
