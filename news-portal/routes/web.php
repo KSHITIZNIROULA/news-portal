0474e7c66ac12acd92b3c11623e6d465cc80f2df
KSHITIZNIROULA <niroulachhitiz@gmail.com>
2025-06-01 14:29:11 +0545

    feat: enhance routing for publisher management and article handling

--- news-portal/routes/web.php
+++ news-portal/routes/web.php
@@ -1,15 +1,21 @@
 <?php
 
 use App\Http\Controllers\Admin\AdminArticleController;
+use App\Http\Controllers\Admin\AdminPublisherController;
 use App\Http\Controllers\Admin\AdminUserController;
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Publisher\PublisherArticleController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('article.index');
+    return view('home');
 })->name('home');
+
+Route::get('/', [ArticleController::class, 'index'])->name('articles.index');//temperorily home address
+Route::get('/articles/search', [ArticleController::class, 'search'])->name('articles.search');
+Route::get('/articles/{slug}', [ArticleController::class, 'show'])->name('articles.show');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -30,8 +36,14 @@ Route::prefix('admin')->middleware(['auth', 'role:admin'])->group(function () {
     Route::delete('/users/{user}', [AdminUserController::class, 'destroy'])->name('admin.users.destroy');
 });
 
-Route::get('/', [ArticleController::class, 'index'])->name('articles.index');//temperorily home address
-Route::get('/articles/{slug}', [ArticleController::class, 'show'])->name('articles.show');
+Route::prefix('admin')->middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/publishers', [AdminPublisherController::class, 'index'])->name('admin.publishers.index');
+    Route::get('/publishers/create', [AdminPublisherController::class, 'create'])->name('admin.publishers.create');
+    Route::post('/publishers', [AdminPublisherController::class, 'store'])->name('admin.publishers.store');
+    Route::get('/publishers/{user}/edit', [AdminPublisherController::class, 'edit'])->name('admin.publishers.edit');
+    Route::put('/publishers/{user}', [AdminPublisherController::class, 'update'])->name('admin.publishers.update');
+    Route::delete('/publishers/{user}', [AdminPublisherController::class, 'destroy'])->name('admin.publishers.destroy');
+});
 
 Route::prefix('admin')->middleware(['auth', 'role:admin|publisher'])->group(function () {
     Route::get('/articles', [AdminArticleController::class, 'index'])->name('admin.articles.index');
@@ -48,5 +60,17 @@ Route::prefix('admin')->middleware(['auth','role:admin'])->group(function(){
     Route::post('/categories',[CategoryController::class, 'store'])->name('admin.categories.store');
     Route::delete('/categories/{category}',[CategoryController::class,'destroy'])->name('admin.categories.destroy');
 });
+
+// Publisher Routes (only for publishers)
+Route::prefix('publisher')->middleware(['auth', 'role:publisher'])->group(function () {
+    Route::get('/dashboard', [PublisherArticleController::class, 'dashboard'])->name('publisher.articles.dashboard');
+    Route::get('/articles/create', [PublisherArticleController::class, 'create'])->name('publisher.articles.create');
+    Route::post('/articles', [PublisherArticleController::class, 'store'])->name('publisher.articles.store');
+    Route::get('/articles/{article}', [PublisherArticleController::class, 'show'])->name('publisher.articles.show');
+    Route::get('/articles/{article}/edit', [PublisherArticleController::class, 'edit'])->name('publisher.articles.edit');
+    Route::put('/articles/{article}', [PublisherArticleController::class, 'update'])->name('publisher.articles.update');
+    Route::delete('/articles/{article}', [PublisherArticleController::class, 'destroy'])->name('publisher.articles.destroy');
+});
+
 
 require __DIR__.'/auth.php';
