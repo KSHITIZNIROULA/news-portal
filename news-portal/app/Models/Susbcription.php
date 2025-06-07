5ae1eb6475d8af6342be771ea99fdffc954560e6
KSHITIZNIROULA <niroulachhitiz@gmail.com>
2025-06-07 09:34:44 +0545

    feat: add Subscription model and extend User model with subscription management methods

---
+++ news-portal/app/Models/Susbcription.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+// app/Models/Subscription.php
+class Subscription extends Model
+{
+    protected $fillable = ['user_id', 'publisher_id', 'subscribed_at', 'expires_at', 'status'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function publisher()
+    {
+        return $this->belongsTo(User::class, 'publisher_id');
+    }
+}
